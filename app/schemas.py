e47b0b213ae02fc6a01e99f678d494344a08aaf5
personal <obryanno@msu.edu>
2025-06-13 12:26:43 -0400

    added user and auth models

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,8 @@
 # app/schemas.py
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import Optional
+
+''' Task Object Schemas'''
 
 # Incoming data (client -> task)
 class TaskCreate(BaseModel): 
@@ -21,3 +23,30 @@ class Task(TaskCreate):
 
     class Config: 
         from_attributes = True # convert to JSON
+
+
+''' User Object Schemas '''
+
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+
+class UserCreate(UserBase):
+    password: str
+
+class UserOut(UserBase):
+    id: int
+
+    class Config: 
+        from_attributes = True # conver to JSON
+
+
+''' User Auth Schemas'''
+
+class UserLogin(BaseModel):
+    username: str
+    pasword: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
